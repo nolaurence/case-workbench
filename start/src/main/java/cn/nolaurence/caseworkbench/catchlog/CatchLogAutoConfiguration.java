31b384a35a896e3299d366db2287d765afe21ec4
Fukang Guo <nolaurence@outlook.com>
2023-02-25 11:45:10 +0800

    add some components

---
+++ start/src/main/java/cn/nolaurence/caseworkbench/catchlog/CatchLogAutoConfiguration.java
@@ -0,0 +1,22 @@
+package cn.nolaurence.caseworkbench.catchlog;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * @Author: nolaurence
+ * @Description: CatchLogAutoConfiguration
+ * @Date: 24/2/2023
+ */
+@Configuration
+@EnableAspectJAutoProxy
+public class CatchLogAutoConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean(CatchLogAspect.class)
+    public CatchLogAspect catchLogAspect() {
+        return new CatchLogAspect();
+    }
+}
